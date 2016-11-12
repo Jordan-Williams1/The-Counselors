db03e864f92cf221172c9057680af0f9369465ea
Steven Nagy <geiganax@gmail.com>
2016-11-12 13:23:05 -0600

    latest sql off server

--- setupIDPDatabase.sql
+++ setupIDPDatabase.sql
@@ -8,78 +8,73 @@ DROP TABLE IF EXISTS behaviors;
 DROP TABLE IF EXISTS consequences;
 DROP TABLE IF EXISTS rewards;
 DROP TABLE IF EXISTS Event;
+SET foreign_key_checks = 1;
 
 CREATE TABLE users
-( userID varchar(10),
-username varchar(100) NOT NULL,
-password varchar(100) NOT NULL,
-masterUserID int NOT NULL,
-childrenPriv int NOT NULL,
-childrenIDP boolean NOT NULL,
-childrenBehHist boolean NOT NULL,
-childrenDesc boolean NOT NULL,
-familySchedPriv int NOT NULL,
-behaviorPriv int NOT NULL,
-conRewPriv int NOT NULL,
-PRIMARY KEY (userID)
+( userID varchar(10) primary key,
+username varchar(100),
+password varchar(100),
+masterUserID int,
+childrenPriv int,
+childrenIDP boolean,
+childrenBehHist boolean,
+childrenDesc boolean,
+familySchedPriv int,
+behaviorPriv int,
+conRewPriv int
 );
 
 CREATE TABLE child
-( childID varchar(10),
+( childID varchar(10) primary key,
 userID varchar(10),
-name varchar(100) NOT NULL,
-dateAdded date NOT NULL,
-age int NOT NULL,
-grade varchar(20) NOT NULL,
-ExtrovertIntrovert int NOT NULL, 
-OutgoingShy int NOT NULL,
-LeaderFollower int NOT NULL,
-ActiveCalm int NOT NULL, 
-PlannerImpulsive int NOT NULL,
-CaringUncaring int NOT NULL,
-PersonalityNotes text NOT NULL,
-Strengths text NOT NULL,
-Weaknesses text NOT NULL,
-MaturityLevel text NOT NULL,
-Interests text NOT NULL,
-DisciplineWorked text NOT NULL,
-DisciplineNotWorked text NOT NULL,
-PRIMARY KEY (childID),
-CONSTRAINT fk_userID FOREIGN KEY (userID) references users(userID)
+name varchar(100),
+dateAdded date,
+age int,
+grade varchar(20),
+ExtrovertIntrovert int, 
+OutgoingShy int,
+LeaderFollower int,
+ActiveCalm int, 
+PlannerImpulsive int,
+CaringUncaring int,
+PersonalityNotes text,
+Strengths text,
+Weaknesses text,
+MaturityLevel text,
+Interests text,
+DisciplineWorked text,
+DisciplineNotWorked text,
+CONSTRAINT fk_childID1 FOREIGN KEY (userID) references users(userID)
 );
 
 CREATE TABLE behaviors
-( userID varchar(10),
-childID varchar(10),
+( childIDe varchar(10),
 desiredBehavior varchar(255),
-problemBehavior varchar(255),	
-PRIMARY KEY (desiredBehavior, problemBehavior),
-CONSTRAINT fk_childID1 FOREIGN KEY (childID) references child (childID)
+problemBehavior varchar(255),
+PRIMARY KEY(desiredBehavior,problemBehavior),
+CONSTRAINT fk_childID2 FOREIGN KEY (childIDe) references child(childID)
 );
 
 CREATE TABLE consequences
-( userID varchar(10),
-childID varchar(10),
+( childID varchar(10),
 PBehavior varchar(255),
-name varchar(255),
+name varchar(255) primary key,
 description text NOT NULL,
-PRIMARY KEY (name),
 CONSTRAINT fk_childID3 FOREIGN KEY (childID) references child(childID)
 );
 
 CREATE TABLE rewards
 ( userID varchar(10),
 childID varchar(10),
-name varchar(255),
+name varchar(255) primary key,
 description text NOT NULL,
-PRIMARY KEY (name),
 CONSTRAINT fk_childID4 FOREIGN KEY (childID) references child(childID)
 );
 
 
 CREATE TABLE Event
 ( userID varchar(10),
-eventID varchar(10),
+eventID varchar(10) primary key,
 childIDE varchar(10),
 desiredbehaviorE varchar(255),
 reward varchar(255),
@@ -90,11 +85,8 @@ eventDeadlineTime time NOT NULL,
 eventFinishDate date NOT NULL,
 eventFinishTime time NOT NULL,
 eventDescription text NOT NULL,
-PRIMARY KEY (eventID),
-CONSTRAINT fk_child5 FOREIGN KEY (childIDE) REFERENCES child(childID),
+CONSTRAINT fk_child FOREIGN KEY (childIDE) REFERENCES child(childID),
 CONSTRAINT fk_behaviors FOREIGN KEY (desiredbehaviorE, problembehaviorE) REFERENCES behaviors(desiredBehavior, problemBehavior),
 CONSTRAINT fk_consequence FOREIGN KEY (consequence) REFERENCES consequences(name), 
 CONSTRAINT fk_reward FOREIGN KEY (reward) REFERENCES rewards(name)
 );
-
-SET foreign_key_checks = 1;
