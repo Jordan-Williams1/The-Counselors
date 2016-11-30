6fc2237a52876c47f3debf2f908660948e08fe60
Jordan Williams <jkw12a@acu.edu>
2016-11-30 17:31:36 -0600

    scrollview to newChildDescription and buttons

--- newChildDescription.lua
+++ newChildDescription.lua
@@ -1,5 +1,5 @@
 local composer = require( "composer" )
-
+local widget = require("widget")
 local scene = composer.newScene()
 
 
@@ -26,16 +26,62 @@ end
 
 -- show()
 function scene:show( event )
-    composer.removeScene("desiredBehaviorListLast")
+    composer.removeScene("addChild")
+
     local sceneGroup = self.view
     local phase = event.phase
 
     if ( phase == "will" ) then
         -- Code here runs when the scene is still off screen (but is about to come on screen)
+        -- ScrollView listener
+	
 
-        local background = display.newRect(display.contentWidth/2, display.contentHeight/2, display.contentWidth, display.contentHeight)
+		local function scrollListener( event )
+
+		    local phase = event.phase
+		    if ( phase == "began" ) then print( "Scroll view was touched" )
+		    elseif ( phase == "moved" ) then print( "Scroll view was moved" )
+		    elseif ( phase == "ended" ) then print( "Scroll view was released" )
+		    end
+
+		    -- In the event a scroll limit is reached...
+		    if ( event.limitReached ) then
+		        if ( event.direction == "up" ) then print( "Reached bottom limit" )
+		        elseif ( event.direction == "down" ) then print( "Reached top limit" )
+		        elseif ( event.direction == "left" ) then print( "Reached right limit" )
+		        elseif ( event.direction == "right" ) then print( "Reached left limit" )
+		        end
+		    end
+
+		    return true
+		end
+
+
+
+		-- Create the widget
+		local scrollView = widget.newScrollView(
+		    {
+		        --top = 100,
+		        --left = 10,
+		        width = display.contentWidth,
+		        height = display.contentHeight,
+		        scrollWidth = 600,
+		        scrollHeight = 2000,
+		        listener = scrollListener
+		    }
+		)
+
+		sceneGroup:insert(scrollView)
+
+		
+        
+
+        local background = display.newRect(display.contentWidth/2, display.contentHeight/2, display.contentWidth, display.contentHeight*2)
         background:setFillColor( 0.745098 ,0.745098 ,0.745098)
         sceneGroup:insert(background)
+        scrollView:insert( background )
+
+
 
 
         local defaultField
@@ -60,66 +106,357 @@ function scene:show( event )
         end
 
 
-         local backButton = display.newRect(display.contentWidth/2 - display.contentWidth/2 + 50, display.contentHeight-display.contentHeight + 100, 70, 75)
+        local backButton = display.newRect(display.contentWidth/2 - display.contentWidth/2 + 50, display.contentHeight-display.contentHeight + 100, 70, 75)
         backButton:setFillColor(0.372549, 0.619608, 0.627451)
-        sceneGroup:insert(backButton)
+        scrollView:insert(backButton)
 
-        local pickText = display.newText("Child Description", display.contentWidth/2, display.contentHeight-display.contentHeight + 100, native.systemFont, 50)
+        local pickText = display.newText("New Child Description", display.contentWidth/2, display.contentHeight-display.contentHeight + 100, native.systemFont, 50)
         pickText.size = 40
         pickText:setFillColor(0,0,0)
-        sceneGroup:insert(pickText)
+        scrollView:insert(pickText)
 
          -- Create text field Name
         childName = native.newTextField( display.contentWidth/1.5, display.contentHeight-display.contentHeight + 200, display.contentWidth/2, 75)
-        sceneGroup:insert(childName)
+        scrollView:insert(childName)
         childName:addEventListener( "userInput", textListener )
 
         local childNameText = display.newText("Name: ", display.contentWidth/2-140, display.contentHeight-display.contentHeight + 200, 400, 75)
         childNameText.size = 40
         childNameText:setFillColor(0,0,0)
-        sceneGroup:insert(childNameText)
+        scrollView:insert(childNameText)
 
         -- Create text field Age
         childAge = native.newTextField(  display.contentWidth/1.5, display.contentHeight-display.contentHeight + 350, display.contentWidth/2, 75)
-        sceneGroup:insert(childAge)
+        scrollView:insert(childAge)
         childAge:addEventListener( "userInput", textListener )
 
         local childAgeText = display.newText("Age: ", display.contentWidth/2-140, display.contentHeight-display.contentHeight + 350, 400, 75)
         childAgeText.size = 40
         childAgeText:setFillColor(0,0,0)
-        sceneGroup:insert(childAgeText)
+        scrollView:insert(childAgeText)
 
         local GradeText = display.newText("Grade: ", display.contentWidth/2-140, display.contentHeight/2-150, 400, 75)
         GradeText.size = 40
         GradeText:setFillColor(0,0,0)
-        sceneGroup:insert(GradeText)
+        scrollView:insert(GradeText)
 
         -- Create text field Age
         childGrade = native.newTextField(  display.contentWidth/1.5, display.contentHeight/2-150, display.contentWidth/2, 75)
-        sceneGroup:insert(childGrade)
+        scrollView:insert(childGrade)
         childGrade:addEventListener( "userInput", textListener )
 
         local DateText = display.newText("Date: ", display.contentWidth/2-140, display.contentHeight/2, 400, 75)
         DateText.size = 40
         DateText:setFillColor(0,0,0)
-        sceneGroup:insert(DateText)
+        scrollView:insert(DateText)
 
         dateField = native.newTextField(  display.contentWidth/1.5, display.contentHeight/2, display.contentWidth/2, 75)
-        sceneGroup:insert(dateField)
+        scrollView:insert(dateField)
         dateField:addEventListener( "userInput", textListener )
 
+        -- function for all radion buttons
+        local function onSwitchPress( event )
+		    local switch = event.target
+		    print( "Switch with ID '"..switch.id.."' is on: "..tostring(switch.isOn) )
+		end
+
+-----------------------------------------------------------------------		
+		-- NEW GROUP EXTROVERT OR INTROVERT
+-----------------------------------------------------------------------	
+
+		-- Create a group for the radio button set
+		local extrovertIntrovert = display.newGroup()
+		scrollView:insert(extrovertIntrovert)
 
 
+		-- Create two associated radio buttons (inserted into the same display group)
+		local extrovert = widget.newSwitch(
+		    {
+		        left = 150,
+		        top = 200,
+		        style = "radio",
+		        id = "extrovert",
+		        initialSwitchState = true,
+		        onPress = onSwitchPress
+		    }
+		)
+		extrovert.x = display.contentWidth/2 - 320 
+		extrovert.y = display.contentWidth/2 + 420 
+		extrovertIntrovert:insert( extrovert )
+
+		local extrovertText = display.newText("extrovert", display.contentWidth/2 - 220, display.contentWidth/2 + 420 )--, 400, 75)
+        extrovertText.size = 30
+        extrovertText:setFillColor(0,0,0)
+        scrollView:insert(extrovertText)
+
+		
+
+		local introvert = widget.newSwitch(
+		    {
+		        left = 250,
+		        top = 200,
+		        style = "radio",
+		        id = "introvert",
+		        onPress = onSwitchPress
+		    }
+		)
+
+		introvert.x = display.contentWidth/2 - 320 
+		introvert.y = display.contentWidth/2 + 500 
+		extrovertIntrovert:insert( introvert )
+
+		local introvertText = display.newText("introvert", display.contentWidth/2 - 220, display.contentWidth/2 + 500 )--, 400, 75)
+        introvertText.size = 30
+        introvertText:setFillColor(0,0,0)
+        scrollView:insert(introvertText)
+		
+
+-----------------------------------------------------------------------		
+		-- NEW GROUP ACTIVE OR CALM
+-----------------------------------------------------------------------		
+
+		local activeCalm = display.newGroup()
+		scrollView:insert(activeCalm)
+
+		local active = widget.newSwitch(
+		    {
+		        left = 150,
+		        top = 200,
+		        style = "radio",
+		        id = "active",
+		        initialSwitchState = true,
+		        onPress = onSwitchPress
+		    }
+		)
+		active.x = display.contentWidth/2 - 320 
+		active.y = display.contentWidth/2 + 620 
+		activeCalm:insert( active )
+
+		local activeText = display.newText("active", display.contentWidth/2 - 220, display.contentWidth/2 + 620 )--, 400, 75)
+        activeText.size = 30
+        activeText:setFillColor(0,0,0)
+        scrollView:insert(activeText)
+		
+		
+
+		local calm = widget.newSwitch(
+		    {
+		        left = 250,
+		        top = 200,
+		        style = "radio",
+		        id = "calm",
+		        onPress = onSwitchPress
+		    }
+		)
+
+		calm.x = display.contentWidth/2 - 320 
+		calm.y = display.contentWidth/2 + 700 
+		activeCalm:insert( calm )
+		
+		local calmText = display.newText("calm", display.contentWidth/2 - 220, display.contentWidth/2 + 700 )--, 400, 75)
+        calmText.size = 30
+        calmText:setFillColor(0,0,0)
+        scrollView:insert(calmText)
+
+-----------------------------------------------------------------------		
+		-- NEW GROUP OUTGOING OR SHY
+-----------------------------------------------------------------------
+	
+		local outgoingShy = display.newGroup()
+		scrollView:insert(outgoingShy)
+
+		local outgoing = widget.newSwitch(
+		    {
+		        left = 150,
+		        top = 200,
+		        style = "radio",
+		        id = "outgoing",
+		        initialSwitchState = true,
+		        onPress = onSwitchPress
+		    }
+		)
+		outgoing.x = display.contentWidth/2 - 80 
+		outgoing.y = display.contentWidth/2 + 420 
+		outgoingShy:insert( outgoing )
+
+		local outgoingText = display.newText("outgoing", display.contentWidth/2 + 20, display.contentWidth/2 + 420 )
+        outgoingText.size = 30
+        outgoingText:setFillColor(0,0,0)
+        scrollView:insert(outgoingText)
+		
+
+		local shy = widget.newSwitch(
+		    {
+		        left = 250,
+		        top = 200,
+		        style = "radio",
+		        id = "shy",
+		        onPress = onSwitchPress
+		    }
+		)
+
+		shy.x = display.contentWidth/2 - 80 
+		shy.y = display.contentWidth/2 + 500 
+		outgoingShy:insert( shy )
+
+		local shyText = display.newText("shy", display.contentWidth/2 + 20, display.contentWidth/2 + 500 )
+        shyText.size = 30
+        shyText:setFillColor(0,0,0)
+        scrollView:insert(shyText)
+
+-----------------------------------------------------------------------		
+		-- NEW GROUP PLANNER OR IMPULSIVE
+-----------------------------------------------------------------------
+
+		local plannerImpulsive = display.newGroup()
+		scrollView:insert(plannerImpulsive)
+
+		local planner = widget.newSwitch(
+		    {
+		        left = 150,
+		        top = 200,
+		        style = "radio",
+		        id = "planner",
+		        initialSwitchState = true,
+		        onPress = onSwitchPress
+		    }
+		)
+		planner.x = display.contentWidth/2 - 80 
+		planner.y = display.contentWidth/2 + 620 
+		plannerImpulsive:insert( planner )
+
+		local plannerText = display.newText("planner", display.contentWidth/2 + 20, display.contentWidth/2 + 620 )
+        plannerText.size = 30
+        plannerText:setFillColor(0,0,0)
+        scrollView:insert(plannerText)
+		
+
+		local impulsive = widget.newSwitch(
+		    {
+		        left = 250,
+		        top = 200,
+		        style = "radio",
+		        id = "impulsive",
+		        onPress = onSwitchPress
+		    }
+		)
+
+		impulsive.x = display.contentWidth/2 - 80 
+		impulsive.y = display.contentWidth/2 + 700 
+		plannerImpulsive:insert( impulsive )
+
+		local impulsiveText = display.newText("impulsive", display.contentWidth/2 + 20, display.contentWidth/2 + 700 )
+        impulsiveText.size = 30
+        impulsiveText:setFillColor(0,0,0)
+        scrollView:insert(impulsiveText)
+
+-----------------------------------------------------------------------		
+		-- NEW GROUP LEADER OR FOLLOWER
+-----------------------------------------------------------------------
+		
+		local leaderFollower = display.newGroup()
+		scrollView:insert(leaderFollower)
+
+		local leader = widget.newSwitch(
+		    {
+		        left = 150,
+		        top = 200,
+		        style = "radio",
+		        id = "leader",
+		        initialSwitchState = true,
+		        onPress = onSwitchPress
+		    }
+		)
+		leader.x = display.contentWidth/2 + 150 
+		leader.y = display.contentWidth/2 + 420 
+		leaderFollower:insert( leader )
+
+		local leaderText = display.newText("leader", display.contentWidth/2 + 250, display.contentWidth/2 + 420 )
+        leaderText.size = 30
+        leaderText:setFillColor(0,0,0)
+        scrollView:insert(leaderText)
+
+		
+
+		local follower = widget.newSwitch(
+		    {
+		        left = 250,
+		        top = 200,
+		        style = "radio",
+		        id = "follower",
+		        onPress = onSwitchPress
+		    }
+		)
+
+		follower.x = display.contentWidth/2 + 150 
+		follower.y = display.contentWidth/2 + 500 
+		leaderFollower:insert( follower )
+
+		local followerText = display.newText("follower", display.contentWidth/2 + 250, display.contentWidth/2 + 500 )
+        followerText.size = 30
+        followerText:setFillColor(0,0,0)
+        scrollView:insert(followerText)
 
 
+-----------------------------------------------------------------------		
+		-- NEW GROUP CARING OR UNCARING
+-----------------------------------------------------------------------
 
-        
+        local caringUncaring = display.newGroup()
+		scrollView:insert(caringUncaring)
+
+		local caring = widget.newSwitch(
+		    {
+		        left = 150,
+		        top = 200,
+		        style = "radio",
+		        id = "caring",
+		        initialSwitchState = true,
+		        onPress = onSwitchPress
+		    }
+		)
+		caring.x = display.contentWidth/2 + 150 
+		caring.y = display.contentWidth/2 + 620 
+		caringUncaring:insert( caring )
+		
+
+		local caringText = display.newText("caring", display.contentWidth/2 + 250, display.contentWidth/2 + 620 )
+        caringText.size = 30
+        caringText:setFillColor(0,0,0)
+        scrollView:insert(caringText)
+
+		local uncaring = widget.newSwitch(
+		    {
+		        left = 250,
+		        top = 200,
+		        style = "radio",
+		        id = "uncaring",
+		        onPress = onSwitchPress
+		    }
+		)
+
+		uncaring.x = display.contentWidth/2 + 150 
+		uncaring.y = display.contentWidth/2 + 700 
+		caringUncaring:insert( uncaring )
+
+
+		local uncaringText = display.newText("uncaring", display.contentWidth/2 + 250, display.contentWidth/2 + 700 )
+        uncaringText.size = 30
+        uncaringText:setFillColor(0,0,0)
+        scrollView:insert(uncaringText)
 
          function backButton:tap(event)
 
             composer.gotoScene("addChild")
+            --childAge:removeSelf()
+            --childGrade:removeSelf()
         
         end
+
+        
+
+
+		scrollView:setScrollHeight(2000)
 
         -- this listens to see if object has been tapped
         backButton:addEventListener("tap", backButton)
