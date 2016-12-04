c58df010c0d615f376c3c123db2fac85ae706913
geigan <geiganax@gmail.com>
2016-12-04 06:35:26 -0600

    d

---
+++ PHP/consequences.php
@@ -0,0 +1,35 @@
+<?php
+//echo $test;
+$jsonC = json_decode($_POST['json'],true);
+session_id($jsonC['params']['session_ID']);
+session_start();
+$con = mysqli_connect('localhost','root','ITC4602016');
+$username = $_SESSION['username'];
+$password = $_SESSION['password'];
+$_SESSION['consequences'] = $jsonC;
+
+echo json_encode($jsonC);
+
+if(!$con) //if connection not established
+{
+	die('Could not connect: '.mysqli_error($con));
+}
+
+mysqli_select_db($con,'IDP')or die('cannot select db');
+$finduser = "select * from users where username='$username' and password='$password';";
+$results = mysqli_query($con,$finduser);
+$userArray = $results->fetch_row();
+$count = mysqli_num_rows($results);
+
+if($count == 1)
+{
+	//$array = ["session_id"=>session_id()];
+	//$array["behaviors"] = $_SESSION["behaviors"];
+	//echo json_encode($array);
+}
+else
+{
+	session_destroy();
+}
+
+?>
